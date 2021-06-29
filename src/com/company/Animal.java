1334dccc0af265a233049f5bd2e40718fac4d5d1
lane froehlich <elaine.froehlich@gmail.com>
2021-06-28 20:53:32 -0400

    created Animal class

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Animal {
+    public int legs;
+    public string size;
+    public String name;
+    public boolean isFixed;
+
+    public Animal(int legs, String size, boolean isFixed, String name) {
+        this.legs = legs;
+        this.size = size;
+        this.name = name;
+        this.isFixed = isFixed;
+    }
 }
