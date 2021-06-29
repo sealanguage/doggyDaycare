78c3acca0af8bf16092c1cb8473e2cfd93b1caac
lane froehlich <elaine.froehlich@gmail.com>
2021-06-29 18:09:22 -0400

    adding dog class.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -5,11 +5,18 @@ public class Animal {
     public String size;
     public String name;
     public boolean isFixed;
+    public double weight;
 
-    public Animal(int legs, String size, boolean isFixed, String name) {
+    public Animal(int legs, String size, boolean isFixed, String name. double weight) {
         this.legs = legs;
         this.size = size;
         this.name = name;
         this.isFixed = isFixed;
+        this weight = weight;
     }
+
+    public void speak() {
+        System.out.println("...");
+    }
+
 }
